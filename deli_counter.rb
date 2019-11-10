c6fa361426f0b6f200061ad2af4477695f9237a8
Kathia Mgwaba <mgwaba68@gmail.com>
2019-11-10 17:02:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "#{name}"
+  position = katz_deli.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name) +1} in line."
 
 end
